28c27c43dc324340a305a9b688d22d52a4c1e50d
HP <HP@DESKTOP-3E8FUVN>
2020-08-05 15:49:31 +0530

    Created calculator class

---
+++ EmployeeManagementSystem/src/com/lti/Calculator.java
@@ -0,0 +1,14 @@
+package com.lti;
+
+public class Calculator {
+
+	public static void add(int a, int b){
+		System.out.println("Sum is: "+(a+b));
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     add(10,30);
+	}
+
+}
