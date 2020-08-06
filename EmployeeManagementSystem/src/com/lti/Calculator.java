6b773e10d181f421ff298b267d5129760e6cfa36
HP <HP@DESKTOP-3E8FUVN>
2020-08-06 09:47:29 +0530

    added diff methods

--- EmployeeManagementSystem/src/com/lti/Calculator.java
+++ EmployeeManagementSystem/src/com/lti/Calculator.java
@@ -6,6 +6,10 @@ public class Calculator {
 		System.out.println("Sum is: "+(a+b));
 	}
 	
+	public static void diff(int a, int b){
+		System.out.println("Difference is: "+(a-b));
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
      add(10,30);
